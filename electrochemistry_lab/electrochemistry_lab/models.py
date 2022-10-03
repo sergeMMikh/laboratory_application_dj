7ac19b13f31f0e08a98f2726848f8b7e1d78d167
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-10-03 15:43:21 +0100

    added: models.py

---
+++ electrochemistry_lab/electrochemistry_lab/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=250)
+    surname = models.CharField(max_length=250)
+    telephone_number = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+
+
+class Furnace(models.Model):
+    furnace_name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    serviceable = models.BooleanField()
+    max_temperature = models.PositiveIntegerField()
+    min_temperature = models.PositiveIntegerField()
+    is_clean = models.BooleanField()
