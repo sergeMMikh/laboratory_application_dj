c6d2c5afdbabcbcbee0385a9cd117ae195f277d2
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-10-18 13:46:35 +0100

    added: BookingOfFurnaceInLine to admin.py

--- electrochemistry_lab/admin.py
+++ electrochemistry_lab/admin.py
@@ -1,20 +1,32 @@
 from django.contrib import admin
-from .models import Person, Furnace
+from django.forms import BaseInlineFormSet
+
+from .models import Person, Furnace, BookingOfFurnace
 
 
-# from .models import Phone
-#
-#
-# @admin.register(Phone)
-# class PhoneAdmin(admin.ModelAdmin):
-#     list_display = 'id', 'name', 'price', 'release_date', 'lte_exists'
-#     list_filter = 'name', 'price'
+# class FurnaceUserInlineFormset(BaseInlineFormSet):
+#     ...
+
+
+class BookingOfFurnaceInLine(admin.TabularInline):
+    model = BookingOfFurnace
+    extra = 3
+    # formset = FurnaceUserInlineFormset
+
 
 @admin.register(Person)
-class PhoneAdmin(admin.ModelAdmin):
-    ...
+class RersonAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'surname', 'telephone_number', 'email'
 
 
 @admin.register(Furnace)
-class PhoneAdmin(admin.ModelAdmin):
-    ...
+class FurnaceAdmin(admin.ModelAdmin):
+    list_display = 'furnace_name', 'location', 'max_temperature', 'min_temperature', 'is_clean', 'serviceable'
+    fields = ['location', 'furnace_name', 'max_temperature', 'min_temperature', 'is_clean', 'serviceable']
+    inlines = [BookingOfFurnaceInLine]
+
+
+@admin.register(BookingOfFurnace)
+class BookingOfFurnaceAdmin(admin.ModelAdmin):
+    list_display = 'date', 'furnace', 'person'
+
--- electrochemistry_lab/models.py
+++ electrochemistry_lab/models.py
@@ -7,11 +7,55 @@ class Person(models.Model):
     telephone_number = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f"{self.first_name} {self.surname}"
+
 
 class Furnace(models.Model):
+    # furnace name in laboratory specifications
     furnace_name = models.CharField(max_length=50)
-    location = models.CharField(max_length=50)  # location in laboratory
-    serviceable = models.BooleanField() # tec
-    max_temperature = models.PositiveIntegerField()
-    min_temperature = models.PositiveIntegerField()
-    is_clean = models.BooleanField()
+
+    # location in laboratory
+    location = models.CharField(max_length=50,
+                                verbose_name='location')
+
+    # technical furnace conditions: is it working (Tru) or broken (False)
+    serviceable = models.BooleanField(verbose_name='available to use')
+
+    # the maximum operating temperature
+    max_temperature = models.PositiveIntegerField(verbose_name='max. temperature')
+
+    # the minimum operating temperature
+    min_temperature = models.PositiveIntegerField(verbose_name='min. temperature')
+
+    # is furnace using for clean materials: free from acids, alkaline, transition or volatilizing elements
+    is_clean = models.BooleanField(verbose_name='for clean materials')
+
+    # the current furnace user
+    user = models.ManyToManyField(Person,
+                                  related_name='furnaces',
+                                  through='BookingOfFurnace')
+
+    def __str__(self):
+        return self.furnace_name
+
+
+class BookingOfFurnace(models.Model):
+    date = models.DateField()
+    furnace = models.ForeignKey(
+        Furnace,
+        on_delete=models.CASCADE,
+        related_name='furnace',
+    )
+
+    person = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+        related_name='user',
+    )
+
+    class Meta:
+        constraints = [models.UniqueConstraint(name='unique_booking',
+                                               fields=['date',
+                                                       'person',
+                                                       'furnace'])]
