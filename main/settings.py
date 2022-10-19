4d594c01a217a1baec594eeb5313be469256ac79
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-10-19 12:26:13 +0100

    changed: ALLOWED_HOSTS

--- main/settings.py
+++ main/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = '0cir6)b-3&nij^mfa%7&)7e(i(eaiy8at3cgzen3*!3+1)p)0c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
