ddc5397f21afab9091f2b42ec4f5483381da70e7
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-10-21 13:13:51 +0100

    changed: views.py

--- electrochemistry_lab/views.py
+++ electrochemistry_lab/views.py
@@ -7,7 +7,7 @@ def home_view(request):
     template = '../templates/base.html'
     context = {}
 
-    furnaces = Furnace.objects.all().order_by('furnace_name').order_by('location')
+    furnaces = Furnace.objects.all().order_by('location', 'furnace_name')
 
     context = {'furnaces': furnaces}
 
